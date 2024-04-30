b4c3d79045a28f913ba457eec04a8e37bd6d29ae
chukwudibarrah <118562380+chukwudibarrah@users.noreply.github.com>
2024-04-30 20:22:31 +0100

    updated blog list

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -2,12 +2,12 @@ import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import * as contentful from "contentful";
-import moment from "moment"; // New import for date formatting
+import moment from "moment"; // For date formatting
 import SEO from "../utils/SEO";
 import { trackLinkClick } from "../utils/Analytics";
 
 export default function Journal() {
-  const [posts, setPosts] = useState([]);
+  const [groupedPosts, setGroupedPosts] = useState({});
 
   const handleLinkClick = () => {
     trackLinkClick("Journal links");
@@ -22,21 +22,37 @@ export default function Journal() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        if (posts.length === 0) {
-          const response = await client.getEntries({
-            content_type: "journal",
-            order: 'fields.published', // Sort by published date
-          });
+        const response = await client.getEntries({
+          content_type: "journal",
+          order: "fields.published", // Sort by published date
+        });
 
-          setPosts(response.items);
-        }
+        // Grouping posts by year and month of publication
+        const groupedPostsByYear = response.items.reduce((acc, post) => {
+          const year = moment(post.fields.published).format('YYYY');
+          const month = moment(post.fields.published).format('MMMM');
+
+          if (!acc[year]) {
+            acc[year] = {};
+          }
+
+          if (!acc[year][month]) {
+            acc[year][month] = [];
+          }
+
+          acc[year][month].push(post);
+
+          return acc;
+        }, {});
+
+        setGroupedPosts(groupedPostsByYear);
       } catch (error) {
         console.error("Error fetching Contentful entries:", error);
       }
     };
 
     fetchPosts();
-  }, [client, posts]);
+  }, [client]);
 
   return (
     <div className="min-h-screen w-screen overscroll-none bg-charcoal">
@@ -49,29 +65,35 @@ export default function Journal() {
         url="https://chukwudibarrah.com/journal"
       />
 
-      <div className="grid lg:grid-cols-2 gap-10 font-outfit py-32 justify-items-center cursor-pointer md:mx-28 mx-4">
-        {posts.map((post) => (
-          <NavLink
-            reloadDocument
-            key={post.sys.id}
-            to={`/journal/${post.fields.slug}`}
-            onClick={handleLinkClick}
-          >
-            {post.fields.featuredImage && (
-              <img
-                src={post.fields.featuredImage.fields.file.url}
-                alt="Thumbnail"
-                className="hover:scale-95 transition-all duration-700"
-              />
-            )}
-            <h3 className="text-gray-200 text-2xl md:text-4xl tracking-wide font-outfit my-5">
-              {post.fields.title}
-            </h3>
-            {/* <p className="text-gray-400"> 
-              {moment(post.fields.published).format('D MMMM YYYY')}
-            </p> */}
-          </NavLink>
-        ))}
+      <div className="font-outfit py-32 justify-items-center cursor-pointer md:mx-28 mx-4">
+        {Object.keys(groupedPosts)
+          .sort((a, b) => b - a) // Sort by latest year first
+          .map((year) => (
+            <div key={year}>
+              <h3 className="text-sienna font-outfit text-3xl uppercase py-5">{year}</h3>
+
+              {Object.keys(groupedPosts[year])
+                .sort((a, b) => moment(b, 'MMMM') - moment(a, 'MMMM')) // Sort months in reverse order
+                .map((month) => (
+                  <div key={month}>
+                    <h4 className="uppercase text-sienna font-outfit text-2xl">{month}</h4>
+
+                    <ul className="list-disc list-inside text-gray-200 text-2xl md:text-4xl font-outfit">
+                      {groupedPosts[year][month].map((post) => (
+                        <NavLink
+                          reloadDocument
+                          key={post.sys.id}
+                          to={`/journal/${post.fields.slug}`}
+                          onClick={handleLinkClick}
+                        >
+                          <li className="py-7">{post.fields.title}</li>
+                        </NavLink>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+            </div>
+          ))}
       </div>
     </div>
   );
